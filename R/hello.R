260ef4cef5a21acde7cf652b8e5742f4444e9f84
lukawanjohi <luqato@gmail.com>
2015-04-22 23:12:30 +0300

    Test commit #2

--- R/hello.R
+++ R/hello.R
@@ -1,14 +1,14 @@
-#' hello
+#' habari
 #'
-#' The typical hello world example.
+#' The typical habari world example.
 #'
 #' @param name a name; default is 'world'
-#' @return character a string; default is 'Hello, world!'
+#' @return character a string; default is 'habari, world!'
 #' @author Luka Wanjohi
 #' @family example
 #' @example inst/examples/ex_hello.R
 #' @export
 hello <- function(name = "world") {
   stopifnot(is.character(name))
-  paste0("Hello, ", name, "!")
+  paste0("habari, ", name, "!")
 }
--- tests/testthat/test_hello.R
+++ tests/testthat/test_hello.R
@@ -3,11 +3,11 @@ context("hello")
 test_that("hello function operates well", {
   # The following function should be used to reduce time on CRAN by avoiding running
   # a test since time on CRAN is limited.  skip_on_cran()
-  
-  expect_that(hello() == "Hello, world!", is_true())
-  expect_that(hello(name = "Reinhard") == "Hello, Reinhard!", is_true())
-  
+
+  expect_that(hello() == "habari, world!", is_true())
+  expect_that(hello(name = "Reinhard") == "habari, Reinhard!", is_true())
+
   expect_that(hello(1), throws_error())
   expect_that(hello(TRUE), throws_error())
   expect_that(hello(NULL), throws_error())
-}) 
+})
